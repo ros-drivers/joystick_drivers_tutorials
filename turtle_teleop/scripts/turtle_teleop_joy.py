ab1b0cd607b439e43fccb30763fab7ef76a1b55c
Gassend Blaise <blaise@willowgarage.com>
2010-07-23 19:39:06 +0000

    Copied turtle_teleop to joystick_drivers_tutorials.

---
+++ turtle_teleop/scripts/turtle_teleop_joy.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python                                                                                        
+# Software License Agreement (BSD License)                                                                   
+#                                                                                                            
+# Copyright (c) 2009, Willow Garage, Inc.                                                                    
+# All rights reserved.                                                                                       
+#                                                                                                            
+# Redistribution and use in source and binary forms, with or without                                         
+# modification, are permitted provided that the following conditions                                         
+# are met:                                                                                                   
+#                      
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided                                         
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived                                         
+#    from this software without specific prior written permission.
+#                                                                                                            
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS                                          
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT                                         
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE                                            
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author Melonee Wise 
+
+import roslib; roslib.load_manifest('turtle_teleop')
+import rospy
+import turtlesim 
+import joy
+
+
+def joy_cb(joy):
+  vel = turtlesim.msg.Velocity()
+  vel.angular = a_scale*joy.axes[angular];
+  vel.linear = l_scale*joy.axes[linear];
+  pub.publish(vel);
+
+if __name__ == '__main__':
+
+  name ='turtle_teleop'
+  rospy.init_node(name)
+
+  linear = rospy.get_param('axis_linear', 1)
+  angular = rospy.get_param('axis_angular', 2)
+  l_scale = rospy.get_param('scale_linear', 50.0)
+  a_scale = rospy.get_param('scale_angular', 2.0)
+  
+  sub = rospy.Subscriber("joy", joy.msg.Joy, joy_cb)
+  pub = rospy.Publisher("turtle1/command_velocity", turtlesim.msg.Velocity)
+
+  rospy.spin()
---
+++ turtle_teleop/src/teleop_turtle_joy.cpp
@@ -0,0 +1,51 @@
+#include <ros/ros.h>
+#include <turtlesim/Velocity.h>
+#include <joy/Joy.h>
+
+class TeleopTurtle
+{
+public:
+  TeleopTurtle();
+
+private:
+  void joyCallback(const joy::Joy::ConstPtr& joy);
+  
+  ros::NodeHandle nh_;
+
+  int linear_, angular_;
+  double l_scale_, a_scale_;
+  ros::Publisher vel_pub_;
+  ros::Subscriber joy_sub_;
+  
+};
+
+TeleopTurtle::TeleopTurtle():
+  linear_(1),
+  angular_(2)
+{
+
+  nh_.param("axis_linear", linear_, linear_);
+  nh_.param("axis_angular", angular_, angular_);
+  nh_.param("scale_angular", a_scale_, a_scale_);
+  nh_.param("scale_linear", l_scale_, l_scale_);
+
+  vel_pub_ = nh_.advertise<turtlesim::Velocity>("turtle1/command_velocity", 1);
+  joy_sub_ = nh_.subscribe<joy::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
+}
+
+void TeleopTurtle::joyCallback(const joy::Joy::ConstPtr& joy)
+{
+  turtlesim::Velocity vel;
+  vel.angular = a_scale_*joy->axes[angular_];
+  vel.linear = l_scale_*joy->axes[linear_];
+  vel_pub_.publish(vel);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "teleop_turtle");
+  TeleopTurtle teleop_turtle;
+
+  ros::spin();
+}
+
